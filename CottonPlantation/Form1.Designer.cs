be4085c2f73608b8e83c13f27ad36bd62652ef87
suuubra <112522241+suuubra@users.noreply.github.com>
2024-05-22 13:26:29 +1200

    Add files via upload

---
+++ CottonPlantation/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace CottonPlantation
+{
+    partial class CottonPlantation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlay.AutoSize = true;
+            this.buttonPlay.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.buttonPlay.Location = new System.Drawing.Point(607, 393);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(181, 45);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "&Play";
+            this.buttonPlay.UseVisualStyleBackColor = false;
+            this.buttonPlay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CottonPlantation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "CottonPlantation";
+            this.Text = "Game Mechanics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+    }
+}
+
